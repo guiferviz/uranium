5ff6f9535ef07dd9c4df277e36fc7d4c903e277f
guiferviz <guiferviz@gmail.com>
2020-04-11 20:17:59 +0200

    Updating URL to the 10.0.0 version.

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]) {
     // An LLVM Context is very similar to programming scopes. It gives you
     // isolation. Modules, functions of types defined in one context
     // cannot be used in other contexts.
-    // http://llvm.org/docs/ProgrammersManual.html#achieving-isolation-with-llvmcontext
+    // https://releases.llvm.org/10.0.0/docs/ProgrammersManual.html#achieving-isolation-with-llvmcontext
     llvm::LLVMContext ctx;
     // Top level structure present in LLVM programs.
     // https://releases.llvm.org/10.0.0/docs/LangRef.html#module-structure
