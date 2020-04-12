605e5975a7c5e85d8fb53342fa00a688d77486f5
guiferviz <guiferviz@gmail.com>
2020-04-12 23:28:17 +0200

    Adding void main with the return void statement.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
+#include "llvm/IR/IRBuilder.h"
 
 
 using namespace llvm;
@@ -13,29 +14,46 @@ int main(int argc, char *argv[]) {
     // cannot be used in other contexts.
     LLVMContext ctx;
     // Top level structure present in LLVM programs.
-    // Now using a pointer.
-    Module *mainModule = new Module("uranium_module", ctx);
+    Module mainModule("uranium_module", ctx);
     // This is only for fun. Let's simulate that our code comes from a file
     // called 'main.ura'.
     // Yes, '*.ura' is the extension for Uranium source code files :)
     // If we do not set the source filename the name of the module is going
     // to be used.
-    mainModule->setSourceFileName("main.ura");
+    mainModule.setSourceFileName("main.ura");
 
     // Create void type and "function that returns void" type.
     // All this types need to be defined on the created context.
-    Type *void_type = Type::getVoidTy(ctx);
+    Type *voidType = Type::getVoidTy(ctx);
     // This constructor does not ask for a context because it asks for
     // a type that already has a context associated.
     // The first parameter is the return type of the function.
     // The second parameter is a boolean that indicates if the number of
-    // arguments is variable. Why a function that does not take parameters
-    // needs this? No idea yet.
-    FunctionType *fun_void_type = FunctionType::get(void_type, false);
+    // arguments is variable.
+    FunctionType *funVoidType = FunctionType::get(voidType, false);
+
+    // Create main function associated to the mainModule.
+    // The linkage defines the visibility of that function during the linkage
+    // phase, that is, the phase in which the objects files are merged into
+    // one executable.
+    // We indicate that we want that function to be present in that linkage.
+    Function *mainFun = Function::Create(
+            funVoidType, Function::ExternalLinkage, "main", mainModule);
+
+    // Creating a block to write the body of the function.
+    // While creating functions the order does not matter, a builder is like a
+    // cursor in our text editor, allow us to write a sequence of instructions.
+    IRBuilder<> builder(ctx);
+    // A block is where the builder will write the code.
+    BasicBlock *mainBlock = BasicBlock::Create(ctx, "main_block", mainFun);
+    // Place cursor at the beggining of the main body.
+    builder.SetInsertPoint(mainBlock);
+    // Add "return void;" statement.
+    builder.CreateRetVoid();
 
     // Write the IR code of our empty module to stderr.
     // Why stderr? I don't know indeed...
-    mainModule->dump();
+    mainModule.dump();
 
     return 0;
 }
