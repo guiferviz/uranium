78984e17fa3220477e8b09ccb9450804d76e3652
guiferviz <guiferviz@gmail.com>
2020-04-12 18:13:14 +0200

    Setting origin filename and correcting that the dump method dumps the code in stderr and not stdout.

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,20 @@
 int main(int argc, char *argv[]) {
     // Create the minimum objects needed to generate some working code.
     // An LLVM Context is very similar to programming scopes. It gives you
-    // isolation. Modules, functions of types defined in one context
+    // isolation. Modules, functions and types defined in one context
     // cannot be used in other contexts.
-    // https://releases.llvm.org/10.0.0/docs/ProgrammersManual.html#achieving-isolation-with-llvmcontext
     llvm::LLVMContext ctx;
     // Top level structure present in LLVM programs.
-    // https://releases.llvm.org/10.0.0/docs/LangRef.html#module-structure
     llvm::Module mainModule("uranium_module", ctx);
+    // This is only for fun. Let's simulate that our code comes from a file
+    // called 'main.ura'.
+    // Yes, '*.ura' is the extension for Uranium source code files :)
+    // If we do not set the source filename the name of the module is going
+    // to be used.
+    mainModule.setSourceFileName("main.ura");
 
-    // Write the IR code of our empty module to stdout.
+    // Write the IR code of our empty module to stderr.
+    // Why stderr? I don't know indeed...
     mainModule.dump();
 
     return 0;
